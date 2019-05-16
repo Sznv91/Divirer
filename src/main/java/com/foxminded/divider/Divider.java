7e942f238080d4867884e7f7a2d9f5efe5c8d822
Mihail Sazonov <sazonov91@gmail.com>
2019-05-17 02:36:33 +0300

    rename some variables

--- src/main/java/com/foxminded/divider/Divider.java
+++ src/main/java/com/foxminded/divider/Divider.java
@@ -13,26 +13,27 @@ public class Divider {
 		}
 		int[] numbersOfDividend = new int[dividend.length() - firstNumber.length() + 1];
 		numbersOfDividend[0] = Integer.parseInt(firstNumber.toString());
-		String remainder = dividend.substring(firstNumber.length());
+		String otherNumberInMassive = dividend.substring(firstNumber.length());
 		int positionNumberInMassive = 1;
 		int positionInRemainder = 0;
 		while (positionNumberInMassive < numbersOfDividend.length) {
 			numbersOfDividend[positionNumberInMassive] = Integer
-					.parseInt(remainder.substring(positionInRemainder, positionInRemainder + 1));
+					.parseInt(otherNumberInMassive.substring(positionInRemainder, positionInRemainder + 1));
 			positionNumberInMassive++;
 			positionInRemainder++;
 		}
 
 		// —формировали массив чисел, определили первое число дл€ делител€.
-		Divider fD = new Divider();
+		Divider dividerInstance = new Divider();
 		ArrayList<Integer> result = new ArrayList<Integer>();
 		for (int i = 0; i < numbersOfDividend.length; i++) {
-			int od = fD.findingDivider(numbersOfDividend[i], Integer.parseInt(divider));
-			result.add(od); // Ќо это не точно
-			int tmp = Integer.parseInt(divider) * od;
-			int ost = numbersOfDividend[i] - tmp;
+			int partOfResult = dividerInstance.
+					findingDivider(numbersOfDividend[i], Integer.parseInt(divider));
+			result.add(partOfResult); 
+			int tmp = Integer.parseInt(divider) * partOfResult;
+			int remainder = numbersOfDividend[i] - tmp;
 			StringBuilder sb = new StringBuilder();
-			sb.append(ost);
+			sb.append(remainder);
 			System.out.println(sb.toString());
 			if (i < numbersOfDividend.length - 1) {
 				sb.append(numbersOfDividend[i + 1]);
@@ -43,14 +44,6 @@ public class Divider {
 		for (int i : result) {
 			System.out.println(i + "result");
 		}
-		// дл€ проверки
-		// System.out.println(new Divider().findingDivider(numbersOfDividend[0],
-		// Integer.parseInt(divider)) + " result");
-		//
-		int one = Integer.parseInt(dividend);
-		int two = Integer.parseInt(divider);
-		// int result = one / two;
-		// System.out.println(result);
 	}
 
 	private Integer findingDivider(int dividend, int divider) {
@@ -63,5 +56,4 @@ public class Divider {
 			}
 		}
 	}
-
 }
